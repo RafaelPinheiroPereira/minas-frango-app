409e0e47465f6c79d7669f075b7b7937655b44a1
Rafael Pinheiro <rafaelpinheiropereira@gmail.com>
2018-08-31 09:34:00 -0300

    Commit inicial

---
+++ app/src/main/java/rafaelpinheiro/ufma/com/br/minasfrango/activity/LoginActivity.java
@@ -0,0 +1,138 @@
+package rafaelpinheiro.ufma.com.br.minasfrango.activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import rafaelpinheiro.ufma.com.br.minasfrango.R;
+import util.SessionManager;
+
+public class LoginActivity extends AppCompatActivity {
+		SessionManager session;
+		
+		LinearLayout loginBox;
+		Button submit;
+		ImageView imgLogo;
+		EditText edtMatricula;
+		EditText edtSenha;
+		private static int REQUEST_STORAGE = 112;
+		private static String[] PERMISSIONS = {Manifest.permission.BLUETOOTH,
+						Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_PRIVILEGED, Manifest.permission.INTERNET};
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.activity_login);
+				initView();
+				verificaPermissao();
+				carregaAnimacao();
+		}
+		
+		private void carregaAnimacao() {
+				Animation animTranslate = AnimationUtils.loadAnimation(this, R.anim.translate);
+				animTranslate.setAnimationListener(new Animation.AnimationListener() {
+						
+						@Override
+						public void onAnimationStart(Animation arg0) {
+						}
+						
+						@Override
+						public void onAnimationRepeat(Animation arg0) {
+						}
+						
+						@Override
+						public void onAnimationEnd(Animation arg0) {
+								loginBox.setVisibility(View.VISIBLE);
+								submit.setVisibility(View.VISIBLE);
+								Animation animFade = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade);
+								loginBox.startAnimation(animFade);
+								submit.startAnimation(animFade);
+								
+						}
+				});
+				imgLogo.startAnimation(animTranslate);
+		}
+		
+		private void initView() {
+				loginBox = findViewById(R.id.LoginBox);
+				submit = findViewById(R.id.submit);
+				loginBox.setVisibility(View.GONE);
+				submit.setVisibility(View.GONE);
+				imgLogo = findViewById(R.id.logo);
+				edtMatricula = findViewById(R.id.edtUser);
+				edtSenha = findViewById(R.id.edtSenha);
+		}
+		
+		private void verificaPermissao() {
+				
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+						if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+										checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+								if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+										// Caso o usuário tenha negado a permissão anteriormente, e não tenha marcado o check "nunca mais mostre este alerta"
+										// Podemos mostrar um alerta explicando para o usuário porque a permissão é importante.
+								} else {
+										// Solicita a permissao
+										ActivityCompat.requestPermissions(LoginActivity.this, PERMISSIONS, REQUEST_STORAGE);
+								}
+						}
+				} else {
+						//ver  o que fazer aqui
+				}
+		}
+		
+		@Override
+		public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+				
+				if (requestCode == REQUEST_STORAGE) {
+						if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+								Toast.makeText(this, "Permissão para acessar Arquivo de Dados concedida", Toast.LENGTH_LONG).show();
+								//cria os diretorios
+
+//								try {
+//										managerFile.lerArquivoVendedor();
+//								} catch (IOException e) {
+//										LoginActivity.this.runOnUiThread(new Runnable() {
+//												public void run() {
+//
+//
+//														Toast.makeText(LoginActivity.this, "ARQUIVO DE ENTRADA: VENDEDOR INVÁLIDO", Toast.LENGTH_LONG).show();
+//
+//												}
+//										});
+//								}
+//						}
+						} else {
+								super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+						}
+				}
+				
+		}
+		
+		private void validarAcesso(EditText edtMatricula, EditText edtSenha) {
+				// tenho que chamar a api/rest e validar se der tudo certo crio a sessao do usuario
+				session.createUserLoginSession(edtMatricula.getText().toString(), edtSenha.getText().toString());
+				Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(i);
+				
+				finish();
+				
+		}
+}
+
+		
+
---
+++ app/src/main/java/util/SessionManager.java
@@ -0,0 +1,119 @@
+package util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+import rafaelpinheiro.ufma.com.br.minasfrango.activity.LoginActivity;
+
+/**
+ * Created by 04717299302 on 26/08/2016.
+ */
+public class SessionManager {
+		
+		SharedPreferences pref;
+		SharedPreferences.Editor editor;
+		Context context;
+		int PRIVATE_MODE = 0;
+		private static final String PREFER_NAME = "TrinityMobilePref";
+		private static final String IS_USER_LOGIN = "IsUserLoggedIn";
+		public static final String KEY_MATRICULA = "matricula";
+		public static final String KEY_SENHA = "senha";
+		
+		public SessionManager(Context context) {
+				this.context = context;
+				pref = context.getSharedPreferences(PREFER_NAME, PRIVATE_MODE);
+				editor = pref.edit();
+		}
+		
+		public void createUserLoginSession(String matricula, String senha) {
+				
+				editor.putBoolean(IS_USER_LOGIN, true);
+				
+				// salva matricula
+				editor.putString(KEY_MATRICULA, matricula);
+				
+				// salva senha
+				editor.putString(KEY_SENHA, senha);
+				
+				// commit changes
+				editor.commit();
+		}
+		
+		/**
+		 * Check login method will check user login status
+		 * If false it will redirect user to login page
+		 * Else do anything
+		 */
+		public boolean checkLogin() {
+				// Check login status
+				if (!this.isUserLoggedIn()) {
+						
+						// user is not logged in redirect him to Login Activity
+						Intent i = new Intent(context, LoginActivity.class);
+						
+						// Closing all the Activities from stack
+						i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						
+						// Add new Flag to start new Activity
+						i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						
+						// Staring Login Activity
+						context.startActivity(i);
+						
+						return true;
+				}
+				return false;
+		}
+		
+		/**
+		 * Get stored session data
+		 */
+		public HashMap<String, String> getUserDetails() {
+				
+				//Use hashmap to store user credentials
+				HashMap<String, String> user = new HashMap<String, String>();
+				
+				user.put(KEY_MATRICULA, pref.getString(KEY_MATRICULA, null));
+				
+				user.put(KEY_SENHA, pref.getString(KEY_SENHA, null));
+				
+				// return user
+				return user;
+		}
+		
+		public int getMatricula() {
+				pref = this.context.getSharedPreferences(PREFER_NAME, 0);
+				int matricula = Integer.parseInt(pref.getString(KEY_MATRICULA, ""));
+				return matricula;
+		}
+		
+		/**
+		 * Clear session details
+		 */
+		public void logoutUser() {
+				
+				// Clearing all user data from Shared Preferences
+				editor.clear();
+				editor.commit();
+				
+				// After logout redirect user to Login Activity
+				Intent i = new Intent(context, LoginActivity.class);
+				
+				// Closing all the Activities
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				
+				// Add new Flag to start new Activity
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				
+				// Staring Login Activity
+				context.startActivity(i);
+		}
+		
+		// Check for login
+		public boolean isUserLoggedIn() {
+				return pref.getBoolean(IS_USER_LOGIN, false);
+		}
+}
